e698f5fa155bf2655e1481a0e2c9f372979ddfa4
fenicmoi <phatthalung.it@gmail.com>
2025-12-19 15:53:52 +0700

    feat: add modern search display and styling, and integrate a new ministry filter.

--- code/info_data.php
+++ code/info_data.php
@@ -1,80 +1,103 @@
-<style>
-.card-img-top {
-width: 100%;
-height: 40vh;
-object-fit: cover;
-}
-</style>
 <?php
-session_start(); 
-include ("../connection/StartConnect.php");	
-include "../connection/function.php";
-$g_id = intval($_POST['g_id']);
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+include_once("../connection/StartConnect.php");
+include_once("../connection/function.php");
 
-$sql = "SELECT g.*,d.dep_name,m.m_name FROM govern as g   
-        INNER JOIN depart as d ON dep_id = g.g_dep
-        INNER JOIN ministry as m ON m.m_id = d.dep_minis
-        WHERE g.g_id = $g_id ";
+$g_id = isset($_POST['g_id']) ? intval($_POST['g_id']) : 0;
 
-$result = dbQuery($sql);
-$row = dbFetchArray($result);
-$num_row = dbNumRows($result);
+if ($g_id > 0) {
+    $sql = "SELECT g.*, d.dep_name, m.m_name 
+            FROM govern as g   
+            INNER JOIN depart as d ON d.dep_id = g.g_dep
+            INNER JOIN ministry as m ON m.m_id = d.dep_minis
+            WHERE g.g_id = ?";
 
-if($num_row>0){?>
+    $result = dbQueryPrepared($sql, [$g_id]);
+    $row = dbFetchAssoc($result);
+    $num_row = dbNumRows($result);
 
-<center>
-   <table class="table table-bordered table-hover">
-      <tr>
-          <td colspan=2>
-            <?php   
-                $g_pic = $row['g_pic'];
-                if ($g_pic == ''){
-                    $g_pic = '../image/pic_head/avatar.png';
-                }
-            ?>
-            <center><img class="rounded" width="200" height="250"  src="image/pic_head/<?=$g_pic?>" ></center>
-        </td>
-      </tr>
-      <tr>
-          <td width="35%">ชื่อ-สกุล</td>
-          <td><?php echo $row["g_head_th"];?></td>
-      </tr>
-      <tr>
-          <td>ตำแหน่ง</td>
-          <td><?php echo $row["g_position"];?></td>
-      </tr>
-      <tr>
-          <td>ต้นสังกัด</td>
-          <td><?php echo $row["dep_name"];?></td>
-      </tr>
-      <tr>
-          <td>โทรศัพท์/โทรสาร</td>
-          <td>
-                <?php echo setformat($row["g_phone"]);?>/ <?php echo setformat($row["g_fax"]);?>
-         </td>
-      </tr>
-      <tr>
-          <td><i class="fa fa-mobile"></i>มือถือ</td>
-          <td><?php echo setformat($row["g_mobile"]);?></td>
-      </tr>
-      <tr>
-          <td><i class="fa fa-mobile"></i>HOT LINE</td>
-          <td><?php echo $row["g_hotline"];?></td>
-      </tr>
-      <tr>
-          <td><i class="fa fa-mobile"></i>ที่อยู่</td>
-          <td><?php echo $row["g_add"];?></td>
-      </tr>
-      <tr>
-          <td><i class="fa fa-mobile"></i>วันที่ Update</td>
-          <td><?php echo $row["g_update"];?></td>
-      </tr>
+    if ($num_row > 0) {
+        $g_pic = !empty($row['g_pic']) ? $row['g_pic'] : 'no_pic.jpg';
+        $g_head_th = $row['g_head_th'];
+        $g_position = $row['g_position'];
+        $dep_name = $row['dep_name'];
+        $m_name = $row['m_name'];
+        $g_phone = setformat($row['g_phone']);
+        $g_fax = setformat($row['g_fax']);
+        $g_mobile = setformat($row['g_mobile']);
+        $g_hotline = $row['g_hotline'];
+        $g_add = $row['g_add'];
+        $g_update = $row['g_update'];
+        ?>
+        <div class="p-4">
+            <div class="row">
+                <div class="col-md-4 text-center mb-4 mb-md-0">
+                    <img src="image/pic_head/<?php echo $g_pic; ?>" class="rounded shadow-sm img-fluid border"
+                        style="width: 100%; max-width: 250px; height: auto; object-fit: cover; border-radius: 15px !important;"
+                        onerror="this.src='images/logo-small.png'">
+                    <div class="mt-3">
+                        <?php if ($g_mobile): ?>
+                            <a href="tel:<?php echo preg_replace('/[^0-9]/', '', $g_mobile); ?>"
+                                class="btn btn-success btn-block shadow-sm py-2">
+                                <i class="fa fa-phone mr-2"></i> โทรหาเบอร์มือถือ
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <h4 class="text-primary font-weight-bold mb-1"><?php echo $g_head_th; ?></h4>
+                    <p class="text-muted font-weight-bold mb-3"><?php echo $g_position; ?></p>
 
+                    <div class="detail-card bg-light p-3 rounded mb-3 border-0">
+                        <div class="row mb-2">
+                            <div class="col-sm-4 text-muted small uppercase font-weight-bold">หน่วยงาน</div>
+                            <div class="col-sm-8 font-weight-bold"><?php echo $dep_name; ?></div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-sm-4 text-muted small uppercase font-weight-bold">สังกัด</div>
+                            <div class="col-sm-8 text-primary"><?php echo $m_name; ?></div>
+                        </div>
+                        <hr class="my-3">
+                        <?php if ($g_phone): ?>
+                            <div class="row mb-2">
+                                <div class="col-sm-4 text-muted small uppercase font-weight-bold">โทรศัพท์</div>
+                                <div class="col-sm-8 font-weight-bold"><?php echo $g_phone; ?></div>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($g_fax): ?>
+                            <div class="row mb-2">
+                                <div class="col-sm-4 text-muted small uppercase font-weight-bold">โทรสาร</div>
+                                <div class="col-sm-8"><?php echo $g_fax; ?></div>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($g_hotline): ?>
+                            <div class="row mb-2">
+                                <div class="col-sm-4 text-muted small uppercase font-weight-bold">HOT LINE</div>
+                                <div class="col-sm-8 text-danger font-weight-bold"><?php echo $g_hotline; ?></div>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($g_add): ?>
+                            <div class="row mb-2">
+                                <div class="col-sm-4 text-muted small uppercase font-weight-bold">ที่อยู่</div>
+                                <div class="col-sm-8 small"><?php echo $g_add; ?></div>
+                            </div>
+                        <?php endif; ?>
+                    </div>
 
-   </table>
-</center>
-   
-<?}else{
-  ##code
+                    <div class="mt-3 text-right">
+                        <small class="text-muted italic"><i class="fa fa-clock-o mr-1"></i> ปรับปรุงล่าสุด:
+                            <?php echo $g_update; ?></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    } else {
+        echo '<div class="alert alert-warning text-center m-4">ไม่พบข้อมูลที่ระบุ</div>';
+    }
+} else {
+    echo '<div class="alert alert-danger text-center m-4">รหัสข้อมูลไม่ถูกต้อง</div>';
 }
 ?>
